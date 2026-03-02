9ba6d9c5df184bdab248bbd6458b5416724ca85a
Kailayne karen <KAILAYNEKARENPASSIS@GMAIL.COM>
2026-03-02 12:48:18 -0300

    Add files via upload

---
+++ Livro.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BibliotecaDistribuida
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public int Edicao { get; set; }
+
+        public bool Disponivel { get; set; }
+    }
+}
